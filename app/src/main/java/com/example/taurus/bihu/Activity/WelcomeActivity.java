505d20680b2c1e42e054fe8d89a82cc0fe85f457
TaurusTrue <708938768@qq.com>
2018-03-10 22:57:14 +0800

    加了个欢迎界面

---
+++ app/src/main/java/com/example/taurus/bihu/Activity/WelcomeActivity.java
@@ -0,0 +1,31 @@
+package com.example.taurus.bihu.Activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.taurus.bihu.R;
+
+public class WelcomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_welcome);
+        Handler handler = new Handler();
+        //当计时结束,跳转至主界面
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                WelcomeActivity.this.finish();
+            }
+        }, 3000);
+    }
+}
